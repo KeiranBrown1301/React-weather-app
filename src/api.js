e370da7d02ed3f91deac255569050ec9fc92354c
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-04 17:15:48 +1000

    api call all up and working through search component, so lat long ndcity name

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+    'X-RapidAPI-Key': '80046a9173mshfa6e7d3477c94f9p15a8f3jsn6ffbfd5b118b',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URl = "https://wft-geo-db.p.rapidapi.com/v1/geo"
