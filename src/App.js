cb88beaf92279f52dec28e6d9c12b17f74336839
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-04 15:59:20 +1000

    added api call for openweather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+export default function App() {
 
-function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
 
   return (
     <div className="App">
@@ -11,5 +29,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
