94fbd9c1ffdc8aa7858353aee85631ffba478668
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-04 16:07:23 +1000

    created basic weather component.

---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css';
+import { Card } from 'semantic-ui-react'
+
+
+function Weather() {
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
