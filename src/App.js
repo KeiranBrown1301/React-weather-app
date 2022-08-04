09dc13d002b41adaf542038f3df79486cfabaa98
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-04 15:08:12 +1000

    did a bit of refctoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import React, { useEffect, useState } from 'react'
 
-const [lat, setLat] = useState([]);
-const [long, setLong] = useState([]);
-
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
+
     </div>
   );
 }
