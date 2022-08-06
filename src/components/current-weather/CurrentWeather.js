c2ed32480a9892fdec5809ff632dd6595d6f01c7
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-06 14:59:16 +1000

    added styling for current weather

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -5,10 +5,12 @@ function CurrentWeather() {
    return (
      <div className="weather">
       <div className="top">
-        <p className="city">Melbourne</p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city">Melbourne</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="" />
      </div>
    )
  }
