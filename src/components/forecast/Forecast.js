3ea1393c953f99521b0c035858e2986089afadaf
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-11 12:53:12 +1000

    icons mapped over for 7 day forecast.

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion"
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion"
 
 function Forecast({data}) {
   return (
     <>
       <label className='title'>Daily</label>
       <Accordion allowZeroExpanded>
-        {data.list.splice(0, 7).map((item, index) => {
-          <AccordionItem>
-            <AccordionItemHeading></AccordionItemHeading>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className='daily-item'>
+                  <img alt="weather" className='icon-small' src={`icons/${item.weather[0].icon}.png`} />
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
-        })}
+        ))}
       </Accordion>
     </>
   )
 }
 
-export default Forecast
+export default Forecast;
