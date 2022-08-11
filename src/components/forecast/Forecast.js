d6bb2b6e13e433d3463d2d39d565e2a065361b12
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-11 12:37:04 +1000

    accordion added and data mapped over in forecast component.

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Accordion } from "react-accessible-accordion"
+import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion"
 
-function Forecast() {
+function Forecast({data}) {
   return (
     <>
       <label className='title'>Daily</label>
       <Accordion allowZeroExpanded>
-
+        {data.list.splice(0, 7).map((item, index) => {
+          <AccordionItem>
+            <AccordionItemHeading></AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        })}
       </Accordion>
     </>
   )
